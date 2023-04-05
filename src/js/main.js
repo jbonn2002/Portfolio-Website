57ca9a9ed4dca1ac2513619504b379b72b5a945c
Jordy <jbonn2002@gmail.com>
2023-04-05 16:38:03 -0700

    fixed navbar for iphone

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,5 @@
-// xlarge:   [ '1281px',  '1680px' ],
-// large:    [ '1025px',  '1280px' ],
-// medium:   [ '737px',   '1024px' ],
-// small:    [ '481px',   '736px'  ],
-// xsmall:   [ null,      '480px'  ],
- 	
 
+//Toggle Dropdown Code
 
 const toggleButton = document.querySelector('.toggle-button')
 const toggleButtonIcon = document.querySelector('.toggle-button i')
@@ -13,8 +8,16 @@ const dropDownMenu = document.querySelector('.dropdown-menu')
 toggleButton.onclick = function () {
     dropDownMenu.classList.toggle('open')
     const isOpen = dropDownMenu.classList.contains('open')
-
+    
     toggleButtonIcon.classList = isOpen
     ? 'fa-solid fa-xmark'
     : 'fa-solid fa-bars'
 }
+
+
+
+// xlarge:   [ '1281px',  '1680px' ],
+// large:    [ '1025px',  '1280px' ],
+// medium:   [ '737px',   '1024px' ],
+// small:    [ '481px',   '736px'  ],
+// xsmall:   [ null,      '480px'  ],
