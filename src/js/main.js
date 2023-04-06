b64436d1d7259812f1113ec7585dc245b19ea0ce
Jordy <jbonn2002@gmail.com>
2023-04-06 00:40:26 -0700

    added scroll effect on side navbar, added links to portfolio pictures, made all links open new tabs, and fixed typo

--- src/js/main.js
+++ src/js/main.js
@@ -15,9 +15,77 @@ toggleButton.onclick = function () {
 }
 
 
+//Headers for Scroll Effect
+const headerOne = document.querySelector('.observe-one')
+const headerTwo = document.querySelector('.observe-two')
+const headerThree = document.querySelector('.observe-three')
+const headerFour = document.querySelector('.observe-four')
 
-// xlarge:   [ '1281px',  '1680px' ],
-// large:    [ '1025px',  '1280px' ],
-// medium:   [ '737px',   '1024px' ],
-// small:    [ '481px',   '736px'  ],
-// xsmall:   [ null,      '480px'  ],
+//Sidebar items for Scroll Effect
+const sideItemOne = document.querySelector('.side-item-one')
+const sideItemTwo = document.querySelector('.side-item-two')
+const sideItemThree = document.querySelector('.side-item-three')
+const sideItemFour = document.querySelector('.side-item-four')
+
+//Scroll effect code
+
+const options = {
+    rootMargin: "-220px 0px -355px 0px"
+}
+
+const optionThree = {
+    rootMargin: "200px 0px -200px 0px"
+}
+
+const optionsFour = {
+    rootMargin: "150px 0px -500px 0px"
+}
+
+const observerOne = new IntersectionObserver(function(entries){
+    entries.forEach( entry => {
+        if(entry.intersectionRatio > 0) {
+            sideItemOne.classList.add('active')
+        }else{
+            sideItemOne.classList.remove('active')
+        }
+
+    })
+});
+
+observerOne.observe(headerOne)
+
+const observerTwo = new IntersectionObserver(function(entries){
+    entries.forEach( entry => {
+        if(entry.intersectionRatio > 0) {
+            sideItemTwo.classList.add('active')
+        }else{
+            sideItemTwo.classList.remove('active')
+        }
+    })
+}, options);
+
+observerTwo.observe(headerTwo)
+
+const observerThree = new IntersectionObserver(function(entries){
+    entries.forEach( entry => {
+        if(entry.intersectionRatio > 0) {
+            sideItemThree.classList.add('active')
+        }else{
+            sideItemThree.classList.remove('active')
+        }
+    })
+}, optionThree);
+
+observerThree.observe(headerThree)
+
+const observerFour = new IntersectionObserver(function(entries){
+    entries.forEach( entry => {
+        if(entry.intersectionRatio > 0) {
+            sideItemFour.classList.add('active')
+        }else{
+            sideItemFour.classList.remove('active')
+        }
+    })
+}, optionsFour);
+
+observerFour.observe(headerFour)
